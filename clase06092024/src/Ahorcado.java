2ed38b629eed3ff97af608d7bd236111e06c2cfc
mishelloeiza <dloeizar@miumg.edu.gt>
2024-09-09 11:14:39 -0600

    modificaciòn de logica del juego

---
+++ clase06092024/src/Ahorcado.java
@@ -0,0 +1,23 @@
+
+import java.awt.BorderLayout;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 50259
+ */
+class Ahorcado {
+
+    static void add(NewJFrame.AhorcadoPanel ahorcadoPanel) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setLayout(BorderLayout borderLayout) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ clase06092024/src/palabraMostrada.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 50259
+ */
+class palabraMostrada {
+
+    static void setCharAt(int i, char letra) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
